e5f5ea5b68dd94552549def83b44f1fce2cd7771
Cymaticcz <zopowp@gmail.com>
2025-04-26 16:04:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+kunden_daten = []
+
+@app.post("/upload")
+async def upload(request: Request):
+    global kunden_daten
+    kunden_daten = await request.json()
+    return {"message": f"{len(kunden_daten)} Datensätze empfangen"}
+
+@app.get("/download")
+def download():
+    return kunden_daten
+
+@app.get("/")
+def home():
+    return {"message": "Server läuft ✔️"}
 
